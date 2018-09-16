0298fdb04aa918ee26efb0030564057f2374d862
Adam Hirst <adam@ahirst.com>
2018-09-16 23:59:56 +0100

    Refactoring

--- wrld.time.js
+++ wrld.time.js
@@ -9,9 +9,17 @@ L.Control.TimeSlider = L.Control.extend({
      */
     options: {
         position: 'bottomleft',
-        layers: null,
+        layers: undefined,
+        minValue: 0,
         maxValue: -1,
-        minValue: 0
+    },
+
+    /**
+     * Event listeners
+     */
+    listeners: {
+        mousedown: undefined,
+        mouseup: undefined,
     },
 
     /**
@@ -26,7 +34,15 @@ L.Control.TimeSlider = L.Control.extend({
      * Called when the position of the map is set or changes
      */
     setPosition: function(position) {
-        
+        var map = this._map;
+
+        if (map) map.removeControl(this);
+
+        this.options.position = position;
+
+        if (map) map.addControl(this);
+
+        return this;
     },
 
     /**
@@ -39,6 +55,8 @@ L.Control.TimeSlider = L.Control.extend({
         var sliderComponent = L.DomUtil.create('div', 'time-container', this._container);
         sliderComponent.innerHTML = `Time: [Time stamp]<br><input type="range" id="time-slider" />`;
 
+        // TODO: Stop map from panning when a mousedown event fires, and resume panning on a mouseup.
+
         return sliderComponent;
     },
 
@@ -46,7 +64,33 @@ L.Control.TimeSlider = L.Control.extend({
      * Called when the control is removed from the map
      */
     onRemove: function(map) {
+        // Delete all layers associated with the slider
+        this.options.markers.forEach(marker => {
+            map.removeLayer(marker);
+        });
 
+        // Remove event listeners
+        document.removeEventListener()
+    },
+
+    /**
+     * Initiate the slider
+     */
+    initializeSlider: function(slider) {
+
+        /**
+         * Data points are stored as
+         * {
+         *  "yyyy-mm-dd hh:mm:ss" => [ marker1, marker2 ]
+         * }
+         */
+
+        // TODO: Handle slider change event and call handleSldierChangeEvent
+
+    },
+
+    handleSliderChangeEvent: function(e) {
+        
     }
 });
 
