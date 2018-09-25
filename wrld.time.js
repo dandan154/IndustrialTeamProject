d75702bbb40ac4bedac3d64b06ea764fbe227429
Daniel J McGuinness <dmcguinness@dundee.ac.uk>
2018-09-25 13:51:58 +0100

    Refactored & Added adam's WrldTime

--- wrld.time.js
+++ wrld.time.js
@@ -19,206 +19,243 @@ L.HeatLayer=(L.Layer?L.Layer:L.Class).extend({initialize:function(t,i){this._lat
 */
 !function(){function o(t,i){this.title=t.title,this.stateName=t.stateName?t.stateName:"unnamed-state",this.icon=L.DomUtil.create("span",""),L.DomUtil.addClass(this.icon,"button-state state-"+this.stateName.replace(/(^\s*|\s*$)/g,"")),this.icon.innerHTML=function(t){var i;t.match(/[&;=<>"']/)?i=t:(t=t.replace(/(^\s*|\s*$)/g,""),i=L.DomUtil.create("span",""),0===t.indexOf("fa-")?L.DomUtil.addClass(i,"fa "+t):0===t.indexOf("glyphicon-")?L.DomUtil.addClass(i,"glyphicon "+t):L.DomUtil.addClass(i,t),i=i.outerHTML);return i}(t.icon),this.onClick=L.Util.bind(t.onClick?t.onClick:function(){},i)}L.Control.EasyBar=L.Control.extend({options:{position:"topleft",id:null,leafletClasses:!0},initialize:function(t,i){i&&L.Util.setOptions(this,i),this._buildContainer(),this._buttons=[];for(var s=0;s<t.length;s++)t[s]._bar=this,t[s]._container=t[s].button,this._buttons.push(t[s]),this.container.appendChild(t[s].button)},_buildContainer:function(){this._container=this.container=L.DomUtil.create("div",""),this.options.leafletClasses&&L.DomUtil.addClass(this.container,"leaflet-bar easy-button-container leaflet-control"),this.options.id&&(this.container.id=this.options.id)},enable:function(){return L.DomUtil.addClass(this.container,"enabled"),L.DomUtil.removeClass(this.container,"disabled"),this.container.setAttribute("aria-hidden","false"),this},disable:function(){return L.DomUtil.addClass(this.container,"disabled"),L.DomUtil.removeClass(this.container,"enabled"),this.container.setAttribute("aria-hidden","true"),this},onAdd:function(){return this.container},addTo:function(t){this._map=t;for(var i=0;i<this._buttons.length;i++)this._buttons[i]._map=t;var s=this._container=this.onAdd(t),e=this.getPosition(),n=t._controlCorners[e];return L.DomUtil.addClass(s,"leaflet-control"),-1!==e.indexOf("bottom")?n.insertBefore(s,n.firstChild):n.appendChild(s),this}}),L.easyBar=function(){for(var t=[L.Control.EasyBar],i=0;i<arguments.length;i++)t.push(arguments[i]);return new(Function.prototype.bind.apply(L.Control.EasyBar,t))},L.Control.EasyButton=L.Control.extend({options:{position:"topleft",id:null,type:"replace",states:[],leafletClasses:!0,tagName:"button"},initialize:function(t,i,s,e){this.options.states=[],null!=e&&(this.options.id=e),this.storage={},"object"==typeof arguments[arguments.length-1]&&L.Util.setOptions(this,arguments[arguments.length-1]),0===this.options.states.length&&"string"==typeof t&&"function"==typeof i&&this.options.states.push({icon:t,onClick:i,title:"string"==typeof s?s:""}),this._states=[];for(var n=0;n<this.options.states.length;n++)this._states.push(new o(this.options.states[n],this));this._buildButton(),this._activateState(this._states[0])},_buildButton:function(){if(this.button=L.DomUtil.create(this.options.tagName,""),"button"===this.options.tagName&&this.button.setAttribute("type","button"),this.options.id&&(this.button.id=this.options.id),this.options.leafletClasses&&L.DomUtil.addClass(this.button,"easy-button-button leaflet-bar-part leaflet-interactive"),L.DomEvent.addListener(this.button,"dblclick",L.DomEvent.stop),L.DomEvent.addListener(this.button,"mousedown",L.DomEvent.stop),L.DomEvent.addListener(this.button,"mouseup",L.DomEvent.stop),L.DomEvent.addListener(this.button,"click",function(t){L.DomEvent.stop(t),this._currentState.onClick(this,this._map?this._map:null),this._map&&this._map.getContainer().focus()},this),"replace"==this.options.type)this.button.appendChild(this._currentState.icon);else for(var t=0;t<this._states.length;t++)this.button.appendChild(this._states[t].icon)},_currentState:{stateName:"unnamed",icon:document.createElement("span")},_states:null,state:function(t){return"string"==typeof t?this._activateStateNamed(t):"number"==typeof t&&this._activateState(this._states[t]),this},_activateStateNamed:function(t){for(var i=0;i<this._states.length;i++)this._states[i].stateName==t&&this._activateState(this._states[i])},_activateState:function(t){if(t!==this._currentState){"replace"==this.options.type&&(this.button.appendChild(t.icon),this.button.removeChild(this._currentState.icon)),t.title?this.button.title=t.title:this.button.removeAttribute("title");for(var i=0;i<this._states.length;i++)L.DomUtil.removeClass(this._states[i].icon,this._currentState.stateName+"-active"),L.DomUtil.addClass(this._states[i].icon,t.stateName+"-active");L.DomUtil.removeClass(this.button,this._currentState.stateName+"-active"),L.DomUtil.addClass(this.button,t.stateName+"-active"),this._currentState=t}},enable:function(){return L.DomUtil.addClass(this.button,"enabled"),L.DomUtil.removeClass(this.button,"disabled"),this.button.setAttribute("aria-hidden","false"),this},disable:function(){return L.DomUtil.addClass(this.button,"disabled"),L.DomUtil.removeClass(this.button,"enabled"),this.button.setAttribute("aria-hidden","true"),this},onAdd:function(t){var i=L.easyBar([this],{position:this.options.position,leafletClasses:this.options.leafletClasses});return this._anonymousBar=i,this._container=i.container,this._anonymousBar.container},removeFrom:function(t){return this._map===t&&this.remove(),this}}),L.easyButton=function(){var t=Array.prototype.concat.apply([L.Control.EasyButton],arguments);return new(Function.prototype.bind.apply(L.Control.EasyButton,t))}}();
 
+
 /**
  * wrld.time.js - Time Series visualisation plugin
  */
 
-//LEGACY - EXAMPLE BUTTON
-/*
-L.Control.ViewButton = L.Control.extend({
+/**
+ * Calculates the number of days from a given other date
+ * @param {Date} other the other date
+ */
+Date.prototype.daysFrom = function(other) {
+    return Math.round(Math.abs((this.getTime() - other.getTime())/(24*60*60*1000)));
+}
 
-    options: {
-        position: 'topleft',
-		width: '80px',			//in pixels
-		height: '50px',			//in pixels
-		animateCamera: true,	//determines if the camera instantly snaps to new orientation
-		tilt: 0, 				//Camera Tilt when active
-		heading: 0				//Camera Heading when active
-    },
+/**
+ * Returns the date that is the number of days from this date
+ * @param {Date} days the number of days to add
+ */
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 
-	initialize: function(display, options){
-		this.setDisplayText(display);
-		L.setOptions(this, options);
+class WrldTime {
 
+    constructor(map, data, options = {}) {
+        this._map = map;
+        this.setData(data);
+        this.displaySlider();
+    }
 
-	},
+    /**
+     * Sets the data for the time layer
+     * @param {*} data the GeoJSON data
+     */
+    setData(data) {
+        this.data = data;
 
-	_displayText: "",
+        var _minDate, _maxDate, _steps;
 
-	_buttonState: false,
+        data.features.forEach(feature => {
+            var date = new Date(feature.properties.date);
 
-	_prevCamHeading: 0,
-
-	_prevCamTilt: 0,
-
-	getDisplayText(){
-		return this._displayText;
-	},
-
-	setDisplayText(displayText){
-		this._displayText = displayText;
-	},
-
-	getPrevCamHeading: function(){
-		return this._prevCamHeading;
-	},
-
-	setPrevCamHeading: function(newHeading){
-		this._prevCamHeading = newHeading;
-	},
-
-	getPrevCamTilt: function(){
-		return this._prevCamTilt;
-	},
-
-	setPrevCamTilt: function(newTilt){
-		this._prevCamTilt = newTilt;
-	},
-
-	getButtonState: function(){return this._buttonState;},
-
-	setButtonState: function(newState){this._buttonState = newState;},
-
-    onAdd: function(map){
-
-        var container = L.DomUtil.create('input', 'btn-container', this._container);
-        container.style.width = this.options.width;
-        container.style.height = this.options.height;
-		container.type = 'button';
-		container.value = this.getDisplayText();
-
-        L.DomEvent.addListener(container, 'click', function(){
-
-			if(this.getButtonState() == false){
-
-				this.setPrevCamHeading(map.getCameraHeadingDegrees());
-				this.setPrevCamTilt(map.getCameraTiltDegrees());
-
-				console.log(this.options.animateCamera);
-
-				map.setView(map.getCenter(), map.getZoom(), {
-					headingDegrees: this.options.heading,
-					tiltDegrees: this.options.tilt,
-					animate: this.options.animateCamera
-				});
-
-				this.setButtonState(true);
-
-			}else{
-
-				map.setView(map.getCenter(), map.getZoom(), {
-					headingDegrees: this.getPrevCamHeading(),
-					tiltDegrees: this.getPrevCamTilt(),
-					animate: this.options.animateCamera
-				});
-
-				this.setButtonState(false);
-
-			}
-        }, this)
-
-        L.DomEvent.addListener(container, 'mouseover', function(){
-            //TODO prevent dragging on map when over element
-            //map.dragging = false;
+            if (_minDate == undefined) { _minDate = date; }
+            if (_maxDate == undefined) { _maxDate = date; }
+            
+            if (date <= _minDate) _minDate = date;
+            if (date >= _maxDate) _maxDate = date;
         });
 
-        return container;
+        this._minDate = _minDate;
+
+        this._steps = _minDate.daysFrom(_maxDate);
+
+        this.setupTimeLayer();
     }
-})
-*/
-
-L.Control.TimeSlider = L.Control.extend({
 
     /**
-     * Control options
+     * Display the slider
      */
-    options: {
-        position: 'bottomleft',
-        layers: undefined,
-        minValue: 0,
-        maxValue: -1,
-    },
+    displaySlider() {
+        this._sliderContainer = document.createElement('div');
+        this._slider = document.createElement('input');
 
-    /**
-     * Event listeners
-     */
-    listeners: {
-        mousedown: undefined,
-        mouseup: undefined,
-    },
+        this._slider.type = 'range';
+        this._slider.classList.add('time-slider');
+        this._slider.addEventListener('input', this.setupTimeLayer.bind(this));
+        this._slider.min = 0;
+        this._slider.max = this._steps;
 
-    /**
-     * Initialization
-     */
-    initialize: function(options) {
-        L.Util.setOptions(this, options);
-        this._layer = this.options.layer;
-    },
+        this._sliderContainer.innerHTML = `Time: [Time stamp]<br>`;
+        this._sliderContainer.append(this._slider);
+        this._sliderContainer.classList.add('time-container');
 
-    /**
-     * Called when the position of the map is set or changes
-     */
-    setPosition: function(position) {
-        var map = this._map;
-
-        if (map) map.removeControl(this);
-
-        this.options.position = position;
-
-        if (map) map.addControl(this);
-
-        return this;
-    },
-
-    /**
-     * Called when the control is added to the map
-     */
-    onAdd: function(map) {
-        this.options.map = map;
-
-        // Create the container
-        var sliderComponent = L.DomUtil.create('div', 'time-container', this._container);
-        sliderComponent.innerHTML = `Time: [Time stamp]<br><input type="range" id="time-slider" />`;
-
-        // TODO: Stop map from panning when a mousedown event fires, and resume panning on a mouseup.
-
-        return sliderComponent;
-    },
-
-    /**
-     * Called when the control is removed from the map
-     */
-    onRemove: function(map) {
-        // Delete all layers associated with the slider
-        //this.options.markers.forEach(marker => {
-        //    map.removeLayer(marker);
-        //});
-
-        // Remove event listeners
-        //document.removeEventListener()
-    },
-
-    /**
-     * Initiate the slider
-     */
-    initializeSlider: function(slider) {
-
-        /**
-         * Data points are stored as
-         * {
-         *  "yyyy-mm-dd hh:mm:ss" => [ marker1, marker2 ]
-         * }
-         */
-
-        // TODO: Handle slider change event and call handleSldierChangeEvent
-
-    },
-
-    handleSliderChangeEvent: function(e) {
-
+        this._map._container.parentElement.parentElement.append(this._sliderContainer);
     }
-});
 
-L.control.timeSlider = function(options) {
-    return new L.Control.TimeSlider(options);
-};
+    /**
+     * Setup the time layer
+     */
+    setupTimeLayer() {
+        if (this._layer != undefined) {
+            this._layer.remove();
+        }
+
+        this._layer = L.geoJSON(this.data, {
+            filter: this.shouldShowFeature.bind(this)
+        });
+        this._layer.addTo(this._map);
+    }
+
+    /**
+     * Returns true if the given feature should be displayed
+     * @param {GeoJSON} feature the feature to test
+     */
+    shouldShowFeature(feature) {
+        return (new Date(feature.properties.date)) <= this._minDate.addDays((this._slider) ? this._slider.value : 0);
+    }
+
+}
+
+function wrldTime(map, data, options) {
+    return new WrldTime(map, data, options);
+}
+
+//TODO: CSS - FIX BUTTON STYLING - font awesome
+class WrldTimeUI{
+
+    constructor(map, options={}){
+        //Variables for button
+        this._prevHeading = 0;
+        this._prevTilt = 0;
+        this._newHeading = 0;
+        this._newTilt = 0;
+        this._animateCamera = true;
+
+        this._menu = this.createMenu(); 
+        this._zoom = this.createZoom();
+        this._toggle = this.createToggleBtn(map, this._menu);
+        this._toggle.addTo(map); 
+        this._zoom.addTo(map); 
+        this._isActive = true; 
+    }
+
+    createToggleBtn(map, menu){
+        
+
+
+        var toggle = L.easyButton({
+            id:'toggle',
+            position: 'topleft',
+            states: [{
+              stateName: 'turn-on',
+              icon: 'Turn Heatmap On <i class="fas fa-toggle-off"></i>',
+              title: 'Turn On Heatmap',
+
+              /**
+               * This controls what happens when the button is clicked
+               */
+              onClick: function(control) {
+                
+                //Sets the previous heading and tilt for the camera when heatmap is toggled off
+                this._prevHeading = map.getCameraHeadingDegrees();
+                this._prevTilt = map.getCameraTiltDegrees();
+                
+                //This changes the view of the camera
+                map.setView(map.getCenter(), map.getZoom(), {
+                    headingDegrees: this._newHeading,
+                    tiltDegrees: this._newTilt,
+                    animate: this._animateCamera
+                });
+
+                menu.addTo(map); 
+
+                //Changes the control state
+                control.state('turn-off'); //This is the stateName this button changes to
+              }
+            }, {
+              icon: 'Turn Heatmap Off <i class="fas fa-toggle-on"></i>',
+              stateName: 'turn-off',
+              title: 'Turn Off Heatmap',
+
+              /**
+               * This controls what happens when the button is clicked
+               */
+              onClick: function(control) {
+                  
+                //Sets the view the button toggles
+                map.setView(map.getCenter(), map.getZoom(), {
+                    headingDegrees: this._prevHeading,
+                    tiltDegrees: this._prevTilt,
+                    animate: this._animateCamera
+                });
+
+                menu.remove(map); 
+                
+                //Changes the button state
+                control.state('turn-on'); //This is the stateName this button changes to
+              }
+            }]
+        });
+
+        return toggle; 
+    }
+
+    createMenu(){
+
+        var menu = [
+                
+            //This is the colour button
+            L.easyButton(
+                '<i class="fas fa-paint-brush"></i>',
+                function(){
+                    //TODO edit heatmaps colours
+                },
+                'Change the colours of the heatmap',
+                'colour-change-button'
+            ),
+            
+            //This is the Opacity button
+            L.easyButton(
+                '<i class="fas fa-glasses"></i>',
+                function(){
+                    //TODO edit heatmaps opacity
+                },
+                'Change the Opacity of the heatmap',
+                'opacity-button'
+            ),
+    
+            //This is the manual birds eye view button
+            L.easyButton(
+                '<i class="fas fa-redo-alt"></i>',
+                function(){
+                    map.setView(map.getCenter(), map.getZoom(), {
+                    headingDegrees: this._newHeading,
+                    tiltDegrees: this._newTilt,
+                    animate: this._animateCamera
+                });
+                },
+                'Reset the view',
+                'reset-view'
+            )
+        ];
+    
+        return L.easyBar(menu); 
+    }
+
+    createZoom(){
+        var zoombuttons = L.control.zoom({
+            position:'topright'
+           })
+
+        return zoombuttons; 
+    }
+}
+
+
+function wrldTimeUI(map) {
+    return new WrldTimeUI(map);
+}
