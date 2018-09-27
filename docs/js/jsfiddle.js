661f632a3f443e6bedeeb52798e251f81a7be60a
Adam Hirst <adam@ahirst.com>
2018-09-27 17:44:16 +0100

    JS fiddle

---
+++ docs/js/jsfiddle.js
@@ -0,0 +1,6 @@
+var map = L.Wrld.map("map", "21343d1ce0a398a7244c29e3c619ea37", { 
+    center: [ 56.46, -2.978 ],
+    indoorsEnabled: true
+  });
+  
+  var indoorControl = new WrldIndoorControl("widget-container", map);
